b2eeda8a6bf3c566e0debf4b18509c5d84ff0ec3
Anish87 <anish87vcmp@gmail.com>
2021-06-07 17:10:26 +0530

    Add `discord` route for `api`

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return $request;
+});
+
+Route::post('/discord', function(Request $request){
+    Http::post('https://discord.com/api/webhooks/849538763557175306/UadDmEIiTMeOwnATk4OwevDsgGWbDjCU50qnUYhe5SQnE9tPHEqszGqGYpef0yY9wSRc', [
+        'content' => "**".$request->author."**: ".$request->content.""
+    ]);
+});
+
+Route::get('/discord2', function(Request $request){
+    return $request;
 });
