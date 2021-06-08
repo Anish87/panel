b12ab359b5ceb5c053e8d214c925e2ad68562cbe
Anish87 <anish87vcmp@gmail.com>
2021-06-08 14:17:11 +0530

    Enhance login page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,27 @@
-const password = document.querySelector('#userPass');
-const togglePassword = document.querySelector('#togglePass');
-togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash fa-eye');
+// const password = document.querySelector('#userPass');
+// const togglePassword = document.querySelector('#togglePass');
+// togglePassword.addEventListener('click', function (e) {
+//     // toggle the type attribute
+//     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     // toggle the eye slash icon
+//     this.classList.toggle('bi-eye-slash text-primary');
+// });
+
+$("body").on('click', '#togglePass', function() 
+    {
+        var toggle = document.getElementById('togglePass');
+        if(toggle.innerHTML === "SHOW PASSWORD") toggle.innerHTML = "HIDE PASSWORD";
+        else if(toggle.innerHTML !== "SHOW PASSWORD") toggle.innerHTML = "SHOW PASSWORD";
+    var input = $("#userPass");
+    if (input.attr("type") === "password") 
+    {
+        input.attr("type", "text");
+    } 
+    else 
+    {
+        input.attr("type", "password");
+    }
 });
 
 /* Code for changing active link on clicking */
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <!-- Favicon -->
         <link rel="shortcut icon" type="image/jpg" href="favicon.png"/>
     </head>
@@ -41,8 +42,10 @@
                                     <div class="input-group-text bg-vgreen text-light border-0"><i class="fas fa-lock"></i></div>
                                     </div>
                                     <input name="userPass" type="password" class="form-control border-0 bg-cblack2 cblack-focus text-light" id="userPass" placeholder="Password" required oninvalid="this.setCustomValidity('Please enter a password!')" oninput="this.setCustomValidity('')">
-                                    <div class="input-group-append">
-                                        <a class="input-group-text text-decoration-none border-0 bg-cblack2" href="#" id="togglePass"><i class="far fa-eye"></i></a>
+                                </div>
+                                <div class="input-group mt-1 mb-n2">
+                                    <div class="text-light">
+                                        <small id="togglePass" class="cursor-pointer">SHOW PASSWORD</small>
                                     </div>
                                 </div>
                                 <div class="mt-3 mb-2">
